786a476d786ddd4b49121bce9aac4bdf865ec990
arbazXcode <jmi.arbaz1@gmail.com>
2026-01-20 00:22:57 +0530

    add about page

---
+++ app/(root)/about/page.jsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div className="p-8 text-white">
+      <h1 className="text-3xl font-bold">About CodeArena</h1>
+      <p className="mt-4">This is a LeetCode-style platform.</p>
+    </div>
+  );
+}
